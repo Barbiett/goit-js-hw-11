a5bb8268aef6c90d41d6c24e369e19e6666f28f9
Barbiett <146207003+Barbiett@users.noreply.github.com>
2024-03-31 14:22:42 +0300

    3

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
 import { getPhoto } from './js/pixabay-api.js';
-import { imagesTemplate } from './js/render-functions.js';
-
+import { imageTemplate } from './js/render-functions.js';
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
-
 searchForm.addEventListener('submit', handleSubmit);
-
 function handleSubmit(event) {
   event.preventDefault();
   const query = searchInput.value.trim();
   if (query === '') {
      return iziToast.warning({message: "Please try again! Write something.", position: "topRight", color: "red"})
   }
-
   loader.classList.remove('is-hidden');
-
   getPhoto(query)
     .then(data => {
-      renderImages(data.hits);
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          color: 'red',
+          position: 'topRight',
+        });
+        return;
+      }
+      imageTemplate(data.hits);
     })
     .catch(error => {
       console.error('Error fetching data:', error);
@@ -31,7 +33,7 @@ function handleSubmit(event) {
       loader.classList.add('is-hidden');
     });
 }
-
 function renderImages(images) {
-  gallery.innerHTML = imagesTemplate(images);
-}
+  gallery.innerHTML = imageTemplate(images);
+  searchForm.reset();
+};
